a6fc204f2a4639a55f93849002bf1ecd61fc5e39
Subhrodip Mohanta <contact@subho.xyz>
2021-05-14 18:46:09 +0530

    Initial Commit

---
+++ src/main/java/xyz/subho/oauth2jwt/OAuth2JwtApplication.java
@@ -0,0 +1,13 @@
+package xyz.subho.oauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/subho/oauth2jwt/OAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.subho.oauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
